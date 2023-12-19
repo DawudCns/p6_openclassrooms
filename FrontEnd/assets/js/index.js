ede0d6479f4298c0a448ff71e2aed1a7249b183d
Dawud Cns <davidhc@hotmail.be>
2023-12-19 21:55:47 +0100

    initial commit

---
+++ FrontEnd/assets/js/index.js
@@ -0,0 +1,151 @@
+/**********Variables générales**********/
+
+// 1. Récupérer l'élément DOM qui hébergera les œuvres
+const gallery = document.querySelector(".gallery");
+// 1. Récupérer l'élément DOM qui hébergera les boutons de catégories
+const filter = document.querySelector(".filter");
+
+/**********Fonctions pour récupérer la galerie et les catégories depuis l'API*********/
+
+/**
+ *
+ * @returns fonction qui récupère les œuvres depuis l'API
+ */
+
+// 2. fonction pour récupérer la galerie depuis l'API
+async function getWorks() {
+  const response = await fetch("http://localhost:5678/api/works");
+  return await response.json();
+}
+
+getWorks();
+
+/**
+ *
+ * @returns fonction qui récupère les catégories depuis l'API
+ */
+
+// 2. fonction pour récupérer les categories depuis l'API
+async function getCategories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  return await response.json();
+}
+
+getCategories();
+
+/**********Fonctions pour générer la galerie et les catégories*******
+
+/**
+ * @generator fonction pour générer des œuvres et les afficher sur le site web
+ */
+
+// 3. fonction pour générer la galerie
+function generateWorks(work) {
+  // créer des éléments dédiés pour chaque œuvre
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  // accéder à l'image de chaque œuvre pour configurer ses attributs source et alt
+  img.src = work.imageUrl;
+  img.alt = work.title;
+  const figcaption = document.createElement("figcaption");
+  // ajouter le titre de l'œuvre à figcaption en tant que texte
+  figcaption.innerText = work.title;
+
+  // ajouter chaque élément à son parent
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+
+  // attacher la balise figure à la div galerie
+  gallery.appendChild(figure);
+}
+
+/**
+ * @generator fonction pour générer des catégories et les afficher sur le site web
+ */
+
+// 3. fonction pour générer les catégories
+function generateCategories(category) {
+  // créer des éléments dédiés pour chaque bouton de catégorie
+  const btnFilter = document.createElement("button");
+  // insérer l'ID et le nom dans chaque bouton de catégorie
+  btnFilter.innerText = category.name.toUpperCase();
+  btnFilter.id = category.id;
+  // ajouter la classe "button"
+  btnFilter.classList.add("button");
+
+  // vérifier si la variable filtre n'est pas nulle avant d'effectuer des opérations dessus
+  if (filter) {
+    // ajouter chaque bouton de catégorie à son parent
+    filter.appendChild(btnFilter);
+  }
+}
+
+/**********Fonctions pour afficher les œuvres et les catégories**********/
+
+/**
+ * @async afficher les œuvres dans le DOM
+ */
+
+// 4. fonction pour afficher les oeuvres
+async function displayWorks() {
+  // stocker la réponse API http dans une constante au format JSON
+  const works = await getWorks();
+
+  works.forEach((work) => {
+    generateWorks(work);
+  });
+}
+displayWorks();
+
+/**
+ * @async afficher les catégories dans le DOM
+ */
+
+// 4. fonction pour afficher les catégories
+async function displayCategories() {
+  // stocker la réponse API http dans une constante au format JSON
+  const categories = await getCategories();
+
+  categories.forEach((category) => {
+    generateCategories(category);
+  });
+}
+displayCategories();
+
+/*********fonction pour filtrer la galerie par catégorie de projet**********/
+
+/**
+ * @event
+ */
+
+// 5. fonction pour filter la galerie par catégorie de projet
+async function filterCategories() {
+  const works = await getWorks();
+
+  const filterButtons = document.querySelectorAll(".filter .button");
+
+  // itérer à travers chaque bouton de filtre
+  filterButtons.forEach((filterButton) => {
+    // ajouter un écouteur d'événements à chaque bouton de filtre
+    filterButton.addEventListener("click", (event) => {
+      // récupérer l'ID du bouton de filtre lors d'un clic sur un bouton sélectionné
+      const filterButtonId = event.target.id;
+
+      // supprimer toutes les œuvres dans la galerie à chaque bouton de filtre pour rafraîchir la page et la rendre interactive
+      gallery.innerHTML = "";
+
+      // logique pour les boutons de filtre
+      if (filterButtonId !== "0") {
+        const filteredWorks = works.filter((work) => {
+          return work.categoryId == filterButtonId;
+        });
+        filteredWorks.forEach((work) => {
+          generateWorks(work);
+        });
+      } else {
+        displayWorks();
+      }
+    });
+  });
+}
+filterCategories();
---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,78 @@
+/*********Constantes globales**********/
+
+// 1. Récupérer l'élément DOM du formulaire de connexion
+const loginForm = document.querySelector(".form .connection");
+
+/**********Fonction de connexion et d'authentification des utilisateurs**********/
+/**
+ * @async Fonction pour connecter les utilisateurs
+ */
+
+// 2. Fonction asynchrone pour gérer le processus de connexion
+async function login() {
+  // 3.  récupérer les valeurs saisies par l'utilisateur dans les champs du formulaire de connexion
+  const loginEmail = document.getElementById("email").value;
+  const loginPassword = document.getElementById("password").value;
+
+  // 4. objet utilisateur à insérer dans le corps de la requête POST
+  const user = {
+    email: loginEmail,
+    password: loginPassword,
+  };
+
+  // 5. capturer les erreurs de la requête fetch
+  try {
+    // 6. Effectuer une requête POST avec les informations utilisateur
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    // 7. vérifier la propriété 'ok' de la réponse HTTP et générer une exception en cas d'erreur
+    if (response.ok) {
+      // 8. récupérer les données de la réponse JSON
+      const data = await response.json();
+      const userdata = data.token;
+
+      // 9. stocker le token utilisateur dans localStorage
+      localStorage.setItem("user", userdata);
+
+      // ce code vérifie si le token d'authentification de l'utilisateur (localStorage.user) correspond au token reçu du serveur (tokenUtilisateur)
+      // S'ils correspondent, il redirige l'utilisateur vers la page "edit.html", suggérant que l'utilisateur est authentifié.
+
+      // 10. vérifier la correspondance des tokens et rediriger l'utilisateur si authentifié
+      if (localStorage.user === userdata) {
+        document.location.href = "edit.html";
+      }
+      console.log(user);
+      console.log(localStorage);
+    } else {
+      // 11. Afficjer un message d'erreur en cas d'informations incorrectes
+      document.querySelector(".error").innerHTML =
+        "Erreur dans l’identifiant ou le mot de passe";
+
+      // 12. Lancer une exception en cas d'erreur HTTP
+      throw new Error(
+        "Erreur dans la requête POST. Statut: " + response.status
+      );
+    }
+  } catch (error) {
+    // 13. Gérer les erreurs capturées
+    console.log(error.message);
+  }
+}
+
+/**
+ * @event submit soumission Appeler la fonction de connexion lors de la soumission du formulaire de connexion
+ */
+
+// 14. attacher la fonction de connexion à l'événement de soumission du formulaire
+loginForm.addEventListener("submit", function (event) {
+  event.stopPropagation(); // 15. empêcher la propagation de l'événement de clic
+  event.preventDefault(); // 16.  empêcher la soumission du formulaire de manière traditionnelle
+  login(); // 17. appeler la fonction de connexion lorsque le formulaire est soumis
+});
